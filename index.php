ee04222806a1bb17ced5c83e656f987584da1bf6
Abhay Hanchate <abhayhanchate.work@gmail.com>
2024-10-02 14:31:11 +0530

    created structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <?php include 'header/header.php'; ?>
+
+  </head>
+  <body>
+    <div class="main">
+      <div class="page1">
+        <video
+          autoplay
+          muted
+          loop
+          src="./assets/takealongs/video/12417857_1280_720_30fps.mp4"
+        ></video>
+        <div class="page1text">
+          <nav>
+            <h3>NueroVista</h3>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <script src="./assets/js/script.js"></script>
+  </body>
+</html>
